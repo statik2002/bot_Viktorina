d32bc85ca69c23ed2cbf5cc25902dc960c4a8a1e
statik2002 <statik2002@mail.ru>
2023-02-09 18:10:32 +0800

    Initial commit

--- viktorina.py
+++ viktorina.py
@@ -1,7 +1,30 @@
-
+from functools import reduce
+from operator import concat
+from pprint import pprint
 
 with open('questions/1vs1200.txt', 'r', encoding='koi8-r') as quesion_file:
-    raw = quesion_file.readlines()
+    raw = quesion_file.read().split('\n\n\n')
 
-for line in raw:
-    print(line)
+questions = []
+
+for line in raw[1:10]:
+
+    splited_line = line.split('\n\n')
+    question = {}
+    for lll in splited_line:
+        element = lll.split('\n')
+        if element[0] == 'Ответ:':
+            question['answer'] = element[1]
+        if element[0] == 'Источник:':
+            question['source'] = element[1]
+        if element[0] == 'Автор:':
+            question['author'] = element[1]
+        if 'Вопрос' in element[0]:
+            question['question'] = ''.join(element[1:])
+
+    questions.append(question)
+
+
+pprint(questions[0]['question'])
+
+
