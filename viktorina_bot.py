de8e4fac38b57b4588801c63ce3c4b5da1d9d063
statik2002 <statik2002@mail.ru>
2023-02-09 18:44:53 +0800

    6 step
    1. Добавил считывание файла с вопросами
    2. Добавил обработку кнопки 'Новый вопрос'
    3. Выбор случайного вопроса и отправка его после нажатия 'Новый вопрос'

--- viktorina_bot.py
+++ viktorina_bot.py
@@ -1,9 +1,14 @@
 import logging
 import os
+import random
 
+import telegram
+from functools import partial
 from dotenv import load_dotenv
 from telegram import Update, ForceReply
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackContext
+
+from utils import load_questions
 
 logger = logging.getLogger(__file__)
 
@@ -21,13 +26,22 @@ def help_command(update: Update, context: CallbackContext) -> None:
     update.message.reply_text('Help!')
 
 
-def echo(update: Update, context: CallbackContext) -> None:
-    update.message.reply_text(update.message.text)
+def messages(
+        update: Update,
+        context: CallbackContext,
+        questions) -> None:
+
+    if update.message.text == 'Новый вопрос':
+        question = questions[random.randrange(len(questions))]
+        update.message.reply_text(question['question'])
 
 
 def main() -> None:
     load_dotenv()
     telegram_token = os.environ['TELEGRAM_TOKEN']
+    chat_id = os.environ['CHAT_ID']
+
+    questions = load_questions('questions/1vs1200.txt')
 
     logging.basicConfig(level=logging.ERROR)
     logger.setLevel(logging.DEBUG)
@@ -35,10 +49,23 @@ def main() -> None:
     updater = Updater(telegram_token)
     dispatcher = updater.dispatcher
 
+    bot = telegram.Bot(token=telegram_token)
+    custom_keyboard = [['Новый вопрос', 'Сдаться'],
+                       ['Мой счёт']]
+    reply_markup = telegram.ReplyKeyboardMarkup(custom_keyboard)
+    bot.send_message(chat_id=chat_id,
+                     text="Привет я бот для викторин!",
+                     reply_markup=reply_markup)
+
     dispatcher.add_handler(CommandHandler("start", start))
     dispatcher.add_handler(CommandHandler("help", help_command))
 
-    dispatcher.add_handler(MessageHandler(Filters.text & ~Filters.command, echo))
+    dispatcher.add_handler(
+        MessageHandler(
+            Filters.text & ~Filters.command,
+            partial(messages, questions=questions)
+        )
+    )
 
     updater.start_polling()
 
